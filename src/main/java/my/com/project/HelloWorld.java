4ef7fc6f3500da4008392d914875d1cfe6b4942a
Pedro Henrique <pedrohms412@gmail.com>
2015-08-19 11:17:26 -0300

    commit inicial

---
+++ src/main/java/my/com/project/HelloWorld.java
@@ -0,0 +1,13 @@
+package my.com.project;
+
+public class HelloWorld{
+
+  public HelloWorld(){
+
+  }
+
+  public static void main(String[] args){
+    System.out.println("Hello World !");
+  }
+
+}
